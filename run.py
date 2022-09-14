247e0ed44b3661c2afe8c67d9a0447a15b3a75a0
jve89 <johanvanerkel@gmail.com>
2022-09-14 16:19:17 +0000

    Defining the generate_password function with the input from the get_input function to generate a password of a predefined length

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import string
+import random
+
+
 def get_input():
     """
     Asks the user for input in how many characters 
@@ -11,7 +15,7 @@ def get_input():
         enter_int = input('Enter your number here:')
         
         if validate_input(enter_int):
-            print('Number is valid!')
+            print(f'Your password will be {enter_int} digit(s) long.\n')
             break
     
     return int(enter_int)
@@ -24,10 +28,11 @@ def validate_input(number):
     try:
         if len(number) != 1:
             raise ValueError(
-                f'{number}. Only 1 number is allowed!'
+                f'{number}.\n' 
+                'Only 1 number is allowed!'
             )
 
-        elif int(number) < 1 or int(number) >= 8:
+        elif int(number) < 1 or int(number) > 8:
             raise ValueError(
                 f'{number}.\n' 
                 'Only a number between 1 and 8 is allowed!'
@@ -40,13 +45,23 @@ def validate_input(number):
     return True
 
 
+def generate_password(length):
+    """
+    x
+    """
+    print('Generating your password...')
+    characters = string.ascii_letters + string.digits
+    gen_pass = ''.join(random.choice(characters) for x in range(int(length)))
+    print(gen_pass)
+    
+
 def main_flow():
     """
     Runs through all the functions.
     """
-    data = get_input()
-    return data
-
+    input_number = get_input()
+    generate_password(input_number)
+    
 
 print("Welcome to the random password generator!\n")
 main_flow()
