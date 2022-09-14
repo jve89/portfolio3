aa5009280046758914b427a0e48f27cfd822aeff
jve89 <johanvanerkel@gmail.com>
2022-09-14 13:39:28 +0000

    Defined the get_input function to accept only 1 number between 1 and 8. If something else is given, the validate_input fuction will start to work and throws out an error.

--- run.py
+++ run.py
@@ -1,16 +1,52 @@
-def get_input_data():
+def get_input():
     """
     Asks the user for input in how many characters 
     the random password should get.
     """
+
+    while True:
+        print('Please enter the length your password should be.')
+        print('Your input should only consist of 1 number between 1 and 8.\n')
     
+        enter_int = input('Enter your number here:')
+        
+        if validate_input(enter_int):
+            print('Number is valid!')
+            break
+    
+    return int(enter_int)
+
+
+def validate_input(number):
+    """
+    x
+    """
+    try:
+        if len(number) != 1:
+            raise ValueError(
+                f'{number}. Only 1 number is allowed!'
+            )
+
+        elif int(number) < 1 or int(number) >= 8:
+            raise ValueError(
+                f'{number}.\n' 
+                'Only a number between 1 and 8 is allowed!'
+            )
+        
+    except ValueError as e:
+        print(f'You entered: {e}. Please try again.\n')
+        return False 
+    
+    return True
+
 
 def main_flow():
     """
     Runs through all the functions.
     """
-    get_input_data()
-    
+    data = get_input()
+    return data
+
 
 print("Welcome to the random password generator!\n")
 main_flow()
