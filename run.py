219ce75856e8a03a6ff39009c7e9e1ddcd2b2176
jve89 <johanvanerkel@gmail.com>
2022-09-14 10:16:44 +0000

    Initial commit. Adding welcome message and calling for main_flow function that functions as a guideline throughout the program. Also adding the get_input_data function that collects the information from the user about how many characters the random password should get.

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_input_data():
+    """
+    Asks the user for input in how many characters 
+    the random password should get.
+    """
+    
+
+def main_flow():
+    """
+    Runs through all the functions.
+    """
+    get_input_data()
+    
+
+print("Welcome to the random password generator!\n")
+main_flow()
